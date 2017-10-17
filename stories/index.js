31a03e3fc7339678b0a3b169b467b531d40fad11
VladFiliucov <filiucov@mail.ru>
2017-10-17 15:35:00 +0100

    Set up storybook knob addon.

--- stories/index.js
+++ stories/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, color, boolean } from '@storybook/addon-knobs';
 
 import { Button, Welcome } from '@storybook/react/demo';
+
+addDecorator(withKnobs);
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .addWithInfo(
     'with text',
-    'Simple button with text',
-    () => <Button onClick={action('clicked')}>Hello Button</Button>
+    'Simple button with text', () => (
+      <Button
+        onClick={action('clicked')}
+        disabled={boolean('Disabled', true)}
+        style={{ backgroundColor: color('Background color', '#BADASS') }}
+      >
+        { text('Button label', 'Hello, Button') }
+      </Button>
+    )
   )
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
