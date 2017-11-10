930a967e3c4a045be9a6a3183f58c4b94187aef5
VladFiliucov <filiucov@mail.ru>
2017-11-10 23:28:39 +0000

    Add Images specs.

---
+++ src/components/ui/Atoms/Image.spec.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow, configure  } from 'enzyme';
+import raf from 'utils/tempPolyfills';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import Image from './Image.jsx';
+
+export const imageSpecs = describe('Image', () => {
+  it('Should have image element by default', () => {
+    const output = shallow(<Image />);
+
+    expect(output.find('img').exists()).toBe(true);
+  });
+
+  it('Should have image with site logo if no src provided', () => {
+    const output = shallow(<Image />);
+
+    expect(output.find('img').prop('src')).toBe('dancy-logo-big.png');
+  });
+
+  it('Should have image with `Dancy Logo` alt text if none provided', () => {
+    const output = shallow(<Image />);
+
+    expect(output.find('img').prop('alt')).toBe('Dancy Logo');
+  });
+
+  it('Should apply image details if they are provided', () => {
+    const output = shallow(<Image src='shapeOfYou.jpg' alt='Super Dance' />);
+
+    expect(output.find('img').prop('src')).toBe('shapeOfYou.jpg');
+    expect(output.find('img').prop('alt')).toBe('Super Dance');
+  });
+});
+
