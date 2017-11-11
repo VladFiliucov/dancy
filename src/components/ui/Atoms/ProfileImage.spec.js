27027cbab47850b35304a103ca3a29c68903d0cf
VladFiliucov <filiucov@mail.ru>
2017-11-11 16:51:23 +0000

    Add ProfileImage specs.

---
+++ src/components/ui/Atoms/ProfileImage.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow, configure  } from 'enzyme';
+import raf from 'utils/tempPolyfills';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import ProfileImage from './ProfileImage.js';
+
+export const profileImageSpecs = describe('ProfileImage', () => {
+  it('Should have img element by default', () => {
+    const output = shallow(<ProfileImage />);
+
+    expect(output.find('img').exists()).toBe(true);
+  });
+
+  it('Should have image with default logo if no src value provided', () => {
+    const output = shallow(<ProfileImage />);
+
+    expect(output.find('img').prop('src')).toBe('user_profile.png');
+  });
+
+  it('Should have image with teachers name alt text if none provided', () => {
+    const output = shallow(<ProfileImage />);
+
+    expect(output.find('img').prop('alt')).toBe('Teachers Name');
+  });
+
+  it('Should apply image details if they are provided', () => {
+    const output = shallow(
+      <ProfileImage src='mattStef.jpg' alt='Matt Stefanina' />
+    );
+
+    expect(output.find('img').prop('src')).toBe('mattStef.jpg');
+    expect(output.find('img').prop('alt')).toBe('Matt Stefanina');
+  });
+});
