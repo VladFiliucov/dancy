3b18ed3dd57722637b8788d79a988f359071600d
VladFiliucov <filiucov@mail.ru>
2017-11-15 22:26:40 +0000

    Add TutorialCard styles.

--- src/components/ui/Organisms/TutorialCard.js
+++ src/components/ui/Organisms/TutorialCard.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 import styles from './TutorialCard.css';
 
+import PreviewCard from 'components/ui/Molecules/PreviewCard';
 import TeacherCard from 'components/ui/Molecules/TeacherCard';
 import Image from 'components/ui/Atoms/Image';
 
+import { preview } from 'static/db.js';
+
 const TutorialCard = ({tutorial}) => (
   <div className={styles.tutorialCard}>
-    <div className={styles.grid}>
-      <Image src={tutorial.preview.src} />
-    </div>
+    <PreviewCard preview={preview} className={styles.grid} />
     <div className={styles.grid}>
       <TeacherCard teacher={tutorial.teacher} />
     </div>
