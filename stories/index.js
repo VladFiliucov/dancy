8891ab701db13be2791d708834de13c63b89b5c1
VladFiliucov <filiucov@mail.ru>
2017-11-15 23:16:02 +0000

    Clean up stories index file.

--- stories/index.js
+++ stories/index.js
@@ -12,9 +12,6 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import expect from 'expect';
 
-import { Button, Welcome } from '@storybook/react/demo';
-import App from 'startup/App';
-
 import NavbarStories from './components/ui/shared/Navbar';
 import ButtonStories from './components/ui/Atoms/Button';
 import ImageStories from './components/ui/Atoms/Image';
@@ -26,49 +23,4 @@ import PreviewCardStories from './components/ui/Molecules/PreviewCard';
 
 configure({ adapter: new Adapter() });
 addDecorator(withKnobs);
-
-storiesOf('Welcome', module)
-  .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .addWithInfo(
-    'with text',
-    'Simple button with text', () => (
-      <Button
-        onClick={action('clicked')}
-        disabled={boolean('Disabled', true)}
-        style={{ backgroundColor: color('Background color', '#BADASS') }}
-      >
-        { text('Button label', 'Hello, Button') }
-      </Button>
-    )
-  )
-  .add('with some emoji', () =>
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  );
-
-
-storiesOf('WeolcomeButton', module).add('Hello World', function () {
-  const story =
-    <button onClick={action('Hello World')}>
-      Hello World
-    </button>;
-
-  specs(() => describe('Hello World', function () {
-    it('Should have the Hello World label', function () {
-      const output = mount(story);
-      expect(output.text()).toContain('Hello World');
-    });
-  }));
-
-  return story;
-});
-
-storiesOf('App', module)
-  .addWithInfo(
-    'main heading',
-    'Application main content', () => (
-      <App />
-    )
-  );
 
