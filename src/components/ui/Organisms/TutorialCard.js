48b478146776e2ab2738af6f2c08a078e3fafab5
VladFiliucov <filiucov@mail.ru>
2017-11-16 21:14:03 +0000

    Add PropTypes for TutorialCard.

--- src/components/ui/Organisms/TutorialCard.js
+++ src/components/ui/Organisms/TutorialCard.js
@@ -7,15 +7,36 @@ import PreviewCard from 'components/ui/Molecules/PreviewCard';
 import TeacherCard from 'components/ui/Molecules/TeacherCard';
 import Image from 'components/ui/Atoms/Image';
 
-import { preview } from 'static/db.js';
+const TutorialCard = ({tutorial}) => {
+  if (!tutorial) return 'This tutorial is not available';
 
-const TutorialCard = ({tutorial}) => (
-  <div className={styles.tutorialCard}>
-    <PreviewCard preview={preview} className={styles.grid} />
-    <div className={styles.grid}>
-      <TeacherCard teacher={tutorial.teacher} />
+  return (
+    <div className={styles.tutorialCard}>
+      <PreviewCard preview={tutorial.preview} className={styles.grid} />
+      <div className={styles.grid}>
+        <TeacherCard teacher={tutorial.teacher} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+TutorialCard.propTypes = {
+  tutorial: PropTypes.shape({
+    preview: PreviewCard.propTypes.preview,
+    teacher: PropTypes.object,
+    mainVideo: PropTypes.shape({
+      id: PropTypes.number,
+      tutorialId: PropTypes.number,
+      url: PropTypes.string
+    }),
+    id: PropTypes.number,
+    teacherId: PropTypes.number,
+    title: PropTypes.string,
+    language: PropTypes.string,
+    danceStyle: PropTypes.string,
+    difficulty: PropTypes.string,
+  })
+};
 
 export default TutorialCard;
+
