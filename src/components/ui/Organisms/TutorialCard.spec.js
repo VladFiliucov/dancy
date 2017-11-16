f3a0907c6dac5de51006a640d53e5f34c5a43050
VladFiliucov <filiucov@mail.ru>
2017-11-16 21:28:19 +0000

    TutorialCard specs.

---
+++ src/components/ui/Organisms/TutorialCard.spec.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow, configure } from 'enzyme';
+import raf from 'utils/tempPolyfills';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import { tutorial } from 'static/db.js';
+
+import TutorialCard from './TutorialCard.js';
+
+export const tutorialCardSpecs = describe('TutorialCard', () => {
+  it('Return no tutorial text if no tutorial provided', () => {
+    const output = shallow(<TutorialCard />);
+
+    expect(output.text()).toContain('This tutorial is not available');
+  });
+
+  it('Does not render cards if no tutorial passed in', () => {
+    const output = shallow(<TutorialCard />);
+
+    expect(output.find('PreviewCard').length).toBe(0);
+    expect(output.find('TeacherCard').length).toBe(0);
+  });
+
+  it('Renders PreviewCard if it is provided', () => {
+    const output = shallow(<TutorialCard tutorial={tutorial} />);
+
+    expect(output.find('PreviewCard').length).toBe(1);
+  });
+
+  it('Renders TeacherCard if it is provided', () => {
+    const output = shallow(<TutorialCard tutorial={tutorial} />);
+
+    expect(output.find('TeacherCard').length).toBe(1);
+  });
+});
