93fe600dc931796cd92cb86ee39162955abce357
VladFiliucov <filiucov@mail.ru>
2017-11-17 08:37:23 +0000

    Add another teacher factory instance.

--- src/static/db.js
+++ src/static/db.js
@@ -8,6 +8,16 @@ export const teacher = {
   styles: 'Commercial'
 };
 
+const teacher2 = {
+  id: 2,
+  firstName: 'Brian',
+  lastName: 'Friedman',
+  avatar: 'BrianFriedman.jpg',
+  bio: `Brian Friedman is choreographer of
+  Britney Spears and Janet Jackson.`,
+  styles: 'Commercial'
+};
+
 export const noImgTeacher = {
   id: 1,
   firstName: 'Lyle',
@@ -23,6 +33,20 @@ export const preview = {
   src: 'shapeOfYou.jpg',
   description: 'Song: SHAPE OF YOU - Ed Sheeran',
   url: 'https://youtu.be/lq5oUOKh368'
+};
+
+const preview2 = {
+  id: 2,
+  tutorialId: 2,
+  src: 'BFpreview.jpg',
+  description: 'Brian Friedman s X FACTOR Masterclass Routine',
+  url: 'https://www.youtube.com/watch?v=kQsaCw9pAKI'
+};
+
+const mainVideo2 = {
+  id: 2,
+  tuotrialId: 1,
+  url: 'https://www.youtube.com/watch?v=7b4YXdQbVYc'
 };
 
 export const tutorial = {
@@ -69,7 +93,7 @@ export const tutorials = [
     preview: {
       id: 1,
       tutorialId: 1,
-      thumbnail: 'shapeOfYou.jpg',
+      src: 'shapeOfYou.jpg',
       description: 'Song: SHAPE OF YOU - Ed Sheeran',
       url: 'https://youtu.be/lq5oUOKh368'
     },
@@ -78,6 +102,17 @@ export const tutorials = [
       tuotrialId: 1,
       url: 'https://youtu.be/3DzkXeMFLpw'
     }
+  },
+  {
+    teacher: teacher2,
+    preview: preview2,
+    mainVideo: mainVideo2,
+    id: 2,
+    teacherId: teacher2.id,
+    title: 'X-Factor masterclass routine',
+    language: 'eng',
+    danceStyle: 'commercial',
+    difficulty: 'intermidiate'
   }
 ];
 
