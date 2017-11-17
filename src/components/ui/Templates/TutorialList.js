b39fcf678bd6919e38ab3e6c5eeef316f7bb549f
VladFiliucov <filiucov@mail.ru>
2017-11-17 08:46:59 +0000

    Add TutorialList.propTypes

--- src/components/ui/Templates/TutorialList.js
+++ src/components/ui/Templates/TutorialList.js
@@ -5,9 +5,15 @@ import styles from './TutorialList.css';
 
 import TutorialCard from 'components/ui/Organisms/TutorialCard';
 
-const TutorialList = ({tutorials}) => (
-  tutorials
-    .map(tutorial => <TutorialCard key={tutorial.id} tutorial={tutorial} />)
-);
+const TutorialList = ({tutorials}) => {
+  if (!tutorials) return <h1>Something went wrong!!</h1>;
+
+  return tutorials
+    .map(tutorial => <TutorialCard key={tutorial.id} tutorial={tutorial} />);
+};
+
+TutorialList.propTypes = {
+  tutorials: PropTypes.arrayOf(TutorialCard.propTypes.tutorial)
+};
 
 export default TutorialList;
