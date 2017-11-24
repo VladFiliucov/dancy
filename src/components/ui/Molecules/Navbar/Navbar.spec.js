68fd34cfd9b43b44719fbe3fc7e4cad7516ccf02
VladFiliucov <filiucov@mail.ru>
2017-11-24 21:23:17 +0000

    Add Navbar specs.

--- src/components/ui/Molecules/Navbar/Navbar.spec.js
+++ src/components/ui/Molecules/Navbar/Navbar.spec.js
@@ -1,3 +1,18 @@
-const navbarSpecs = 1
+import React from 'react';
+import expect from 'expect';
+import { shallow, configure } from 'enzyme';
+import 'utils/tempPolyfills';
+import Adapter from 'enzyme-adapter-react-16';
 
-export default navbarSpecs;
+configure({ adapter: new Adapter() });
+
+import Navbar from './Navbar.js';
+
+export const navbarSpecs = describe('Navbar', () => {
+  it('Should have default title', () => {
+    const output = shallow(<Navbar />);
+
+    expect(output.text()).toContain('Dancy');
+  });
+});
+
