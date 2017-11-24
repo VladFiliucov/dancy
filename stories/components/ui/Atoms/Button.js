5e1b66910dc76b2c611cc6ccdefb1d354de85294
VladFiliucov <filiucov@mail.ru>
2017-11-24 22:21:43 +0000

    Fix linting errors in Button story.

--- stories/components/ui/Atoms/Button.js
+++ stories/components/ui/Atoms/Button.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import { storiesOf, addDecorator } from '@storybook/react';
-import { mount, shallow } from 'enzyme';
-import { specs, describe, it } from 'storybook-addon-specifications';
-import expect from 'expect';
+import { specs } from 'storybook-addon-specifications';
 import { withKnobs, text, color, boolean } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
 
