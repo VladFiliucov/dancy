650313069679d8e9d3a94ac430326229cc28bdef
VladFiliucov <filiucov@mail.ru>
2017-11-25 23:01:32 +0000

    Use base config in storybook webpack config.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,43 +1,9 @@
+require('babel-register');
+require('babel-polyfill');
 const path = require('path');
-const root = path.join(process.cwd(), 'src');
+const merge = require('webpack-merge');
+const baseConfig = require('../initializers/config/base.js');
 
 module.exports = (storybookBaseConfig, configType) => {
-  storybookBaseConfig.module.rules.push({
-    test: /\.scss$/,
-    loaders: ["style-loader", "css-loader", "sass-loader"],
-    include: path.resolve(__dirname, '../')
-  });
-
-  storybookBaseConfig.module.rules.push({
-    test: /\.css$/,
-    use: [
-      {
-        loader: 'style-loader',
-      },
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-        },
-      },
-    ],
-  });
-
-  storybookBaseConfig.module.rules.push({
-    test: /\.(js|jsx)?$/,
-    use: {
-      loader: 'babel-loader'
-    },
-    exclude: /node_modules/
-  });
-
-  storybookBaseConfig.resolve = {
-    extensions: ['.js', '.jsx'],
-    modules: [
-      'node_modules',
-      path.resolve(root)
-    ]
-  };
-
-  return storybookBaseConfig;
+  return merge(storybookBaseConfig, baseConfig);
 };
